01a8a684ccfd2a1c53281b3927c8789543832afa
Kasojas <156245225+Kasojas@users.noreply.github.com>
2024-02-14 16:02:45 -0800

    update

--- grade.sh
+++ grade.sh
@@ -14,3 +14,57 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cp student-submission/*.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+if ! [ -f 'ListExamples.java' ]
+then
+    #notInGradingArea=$(find 'ListExamples.java' | wc)
+    #notInGradingArea2=$( echo $notInGradingArea | awk -F'[, ]' '{print $1}')
+    #echo $notInGradingArea2
+    #if [[ $notInGradingArea2 -gt '0']]
+    #then 
+    #    echo "ListExamples.java wrong directory"
+    #    echo "Score: 0"
+    #    exit
+    #else
+    #    echo "ListExamples.java wrong dir"
+    #    echo "Score: 0"
+    #    exit
+    echo "ListExamples.java does not exist"
+        echo "Score: 0"
+        exit
+fi
+
+javac -cp $CPATH *.java 
+if [[ $? -ne 0 ]]
+then
+    echo "Compilation Error"
+    echo "Score: 0"
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
+
+cat output.txt
+
+test=$(cat output.txt | tail -n 2 | head -n 1)
+fails=$(echo $test | grep 'Failures:')
+tests=$(echo $test | grep 'Tests run:')
+testS=$(echo $test | grep 'test)')
+tested1=$(echo $tests | awk -F'[, ]' '{print $3}' )
+tested2=$(echo $testS | awk -F'[( ]' '{print $2}')
+fail=$(echo $test | awk -F'[, ]' '{print $6}')
+#success=$(($tests - $fail))
+
+#echo "Score: $success / $fail"
+
+echo $test
+echo "this"
+echo $tested2
+echo "next"
+echo $fail
